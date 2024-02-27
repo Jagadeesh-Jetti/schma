c1cd8ce6585afd61a9a0d1719d04b0bbd3bf892f
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-02-27 23:53:44 +0530

    feat: added students display page

---
+++ src/pages/Students/students.jsx
@@ -0,0 +1,11 @@
+export const Students = () => {
+  return (
+    <div className="students">
+      <div className="header">
+        <div className="title">Students</div>
+        <div className="add_student">Add Student</div>
+      </div>
+      <div className="students_table"></div>
+    </div>
+  );
+};
