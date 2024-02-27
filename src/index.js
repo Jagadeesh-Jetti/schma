e44351e0ba423f20feea1d6f592aa27739558939
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-02-27 23:56:31 +0530

    feat: wrapped browser router around app

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { Provider } from "react";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
 import reportWebVitals from "./reportWebVitals";
 import store from "./utils/store";
@@ -10,9 +11,11 @@ import store from "./utils/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
