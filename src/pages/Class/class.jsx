c94fdbb465a2f2cf0e1f81332fb49108cf688172
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-02-27 23:55:05 +0530

    feat: added class display page

---
+++ src/pages/Class/class.jsx
@@ -0,0 +1,13 @@
+export const Class = () => {
+  return (
+    <div>
+      <div className="class">
+        <div className="header">
+          <div className="title">Class</div>
+          <div className="filters">gender and sort</div>
+        </div>
+        <div className="class_display"></div>
+      </div>
+    </div>
+  );
+};
