6538a9c755dd8429dc09e8090122245908897cf2
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-02-27 23:53:21 +0530

    feat : added teachers displaypage

---
+++ src/pages/Teachers/teachers.jsx
@@ -0,0 +1,11 @@
+export const Teachers = () => {
+  return (
+    <div className="teachers">
+      <div className="header">
+        <div className="title">Teachers</div>
+        <div className="add_teacher">Add Teacher</div>
+      </div>
+      <div className="teachers_table"></div>
+    </div>
+  );
+};
