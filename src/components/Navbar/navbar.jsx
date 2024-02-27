0ac6c3f0425caca0827ccd2faaa87a874ad23d8e
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-02-27 23:50:58 +0530

    feat : added navbar feature

---
+++ src/components/Navbar/navbar.jsx
@@ -0,0 +1,18 @@
+import "../Navbar/navbar.css";
+import { useNavigate } from "react-router";
+
+export const Navbar = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="navbar_page">
+      <h2 className="title">School Management</h2>
+
+      <div className="options_container">
+        <h3 onClick={() => navigate("/students")}> Student </h3>
+        <h3 onClick={() => navigate("/teachers")}> Teacher </h3>
+        <h3 onClick={() => navigate("/class")}> Class </h3>
+        <h3 onClick={() => navigate("/school")}> School </h3>
+      </div>
+    </div>
+  );
+};
