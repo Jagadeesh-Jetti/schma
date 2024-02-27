f9ca471a06cb08cf351e84610560e5746148c1d0
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-02-27 23:54:14 +0530

    feat: created school page

---
+++ src/pages/School/school.jsx
@@ -0,0 +1,10 @@
+export const School = () => {
+  return (
+    <div className="school">
+      <div className="header">
+        <div className="title">School</div>
+      </div>
+      <div className="school_display"></div>
+    </div>
+  );
+};
