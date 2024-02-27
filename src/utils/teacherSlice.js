06b530373745c8e16c74ddab0a16517d85390ba8
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-02-27 23:59:15 +0530

    feat: initialized teacher slice

--- src/utils/teacherSlice.js
+++ src/utils/teacherSlice.js
@@ -1,31 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
-  totalStudents: 0,
-  averageAttendance: 0,
-  averageMarks: 0,
-  topStudent: null,
-};
-
-export const schoolSlice = createSlice({
-  name: "school",
-  initialState,
-  reducers: {
-    updateState: (state, action) => {
-      const { totalStudents, averageAttendance, averageMarks, topStudent } =
-        action.payload;
-
-      state.topStudent = topStudent;
-      state.averageMarks = averageMarks;
-      state.totalStudents = totalStudents;
-      state.averageAttendance = averageAttendance;
-    },
-
-    setTopStudent: (state, action) => {
-      state.topStudent = action.payload;
-    },
-  },
+export const teacherSlice = createSlice({
+  name: "teachers",
+  reducers: {},
 });
 
-export const { updateState, setTopStudent } = schoolSlice.actions;
-export default schoolSlice.reducer;
+export default teacherSlice.reducer;
