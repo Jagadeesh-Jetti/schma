a24baf3b40f1e897ae5824766252da6fda7cee17
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-02-27 23:58:21 +0530

    feat: added routers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
+import { Routes, Route } from "react-router";
+import { Navbar } from "./components/Navbar/navbar";
+import { Students } from "./pages/Students/students";
+import { Teachers } from "./pages/Teachers/teachers";
+import { Class } from "./pages/Class/class";
+import { School } from "./pages/School/school";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <Navbar />
+      <Routes>
+        <Route path="/students" element={<Students />} />
+        <Route path="/teachers" element={<Teachers />} />
+        <Route path="/class" element={<Class />} />
+        <Route path="/school" element={<School />} />
+      </Routes>
     </div>
   );
 }
