402a78cb8c3985a6ce75edd7dda66dab373f7b9f
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-02-27 23:49:31 +0530

    feat: created and configured store

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import schoolSlice from "./schoolSlice";
+import { schoolSlice } from "./schoolSlice";
+import { teacherSlice } from "./teacherSlice";
+import { studentSlice } from "./studentSlice";
 
 export default configureStore({
   reducer: {
     school: schoolSlice.reducer,
+    teachers: teacherSlice.reducer,
+    students: studentSlice.reducer,
   },
 });
