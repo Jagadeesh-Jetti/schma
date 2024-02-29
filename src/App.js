fa83ec7efa955817a410fa0ed9633fb271240528
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-02-29 15:33:36 +0530

    feat: added routers and connected the app

--- src/App.js
+++ src/App.js
@@ -5,16 +5,29 @@ import { Students } from "./pages/Students/students";
 import { Teachers } from "./pages/Teachers/teachers";
 import { Class } from "./pages/Class/class";
 import { School } from "./pages/School/school";
+import { StudentDetail } from "./pages/StudentDetail/studentDetail";
+import { TeacherDetail } from "./pages/TeacherDetail/teacherDetail";
+import { AddStudent } from "./components/AddStudentForm/addStudentForm";
+import { AddTeacher } from "./components/AddTeacherForm/addTeacherForm";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes>
-        <Route path="/students" element={<Students />} />
+        <Route path="/" element={<Students />} />
         <Route path="/teachers" element={<Teachers />} />
         <Route path="/class" element={<Class />} />
         <Route path="/school" element={<School />} />
+
+        <Route path="student/:id" element={<StudentDetail />} />
+        <Route path="teacher/:id" element={<TeacherDetail />} />
+
+        <Route path="/add-student" element={<AddStudent />} />
+        <Route path="/add-teacher" element={<AddTeacher />} />
+
+        <Route path="student/edit/:id" element={<AddStudent />} />
+        <Route path="teacher/edit/:id" element={<AddTeacher />} />
       </Routes>
     </div>
   );
