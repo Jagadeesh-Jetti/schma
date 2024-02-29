4348cc85d14f7b0016de185cb7d7db51a6501ee3
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-02-29 15:26:48 +0530

    feat: implemented redux slice for school

--- src/utils/schoolSlice.js
+++ src/utils/schoolSlice.js
@@ -1,8 +1,31 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = {
+  totalStudents: 0,
+  averageAttendance: 0,
+  averageMarks: 0,
+  topStudent: null,
+};
+
 export const schoolSlice = createSlice({
   name: "school",
-  reducer: {},
+  initialState,
+  reducers: {
+    updateState: (state, action) => {
+      const { totalStudents, averageAttendance, averageMarks, topStudent } =
+        action.payload;
+
+      state.topStudent = topStudent;
+      state.averageMarks = averageMarks;
+      state.totalStudents = totalStudents;
+      state.averageAttendance = averageAttendance;
+    },
+    setTopStudent: (state, action) => {
+      state.topStudent = action.payload;
+    },
+  },
 });
+
+export const { updateState, setTopStudent } = schoolSlice.actions;
 
 export default schoolSlice.reducer;
