b91b7a330d17bffb042d676b8ccd9e4e4b021178
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-02-29 15:27:46 +0530

    feat: added navbar component

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -8,7 +8,7 @@ export const Navbar = () => {
       <h2 className="title">School Management</h2>
 
       <div className="options_container">
-        <h3 onClick={() => navigate("/students")}> Student </h3>
+        <h3 onClick={() => navigate("/")}> Student </h3>
         <h3 onClick={() => navigate("/teachers")}> Teacher </h3>
         <h3 onClick={() => navigate("/class")}> Class </h3>
         <h3 onClick={() => navigate("/school")}> School </h3>
