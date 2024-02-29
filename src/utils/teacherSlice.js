a5cfb3b6a51e60cff8cb2d6e45d75a1740ece448
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-02-29 15:27:23 +0530

    feat: implemented redux slice for teacher

--- src/utils/teacherSlice.js
+++ src/utils/teacherSlice.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const url = "https://school-management-backend-eosin.vercel.app/teachers";
 
 export const fetchTeachers = createAsyncThunk(
-  "teacher/fetchTeachers",
+  "teachers/fetchTeachers",
   async () => {
     try {
       const response = await axios.get(url);
@@ -16,7 +16,7 @@ export const fetchTeachers = createAsyncThunk(
 );
 
 export const addTeacher = createAsyncThunk(
-  "teacher/add",
+  "teachers/add",
   async (newTeacher) => {
     try {
       const response = await axios.post(url, newTeacher);
@@ -28,7 +28,7 @@ export const addTeacher = createAsyncThunk(
 );
 
 export const updateTeacher = createAsyncThunk(
-  "teacher/update",
+  "teachers/update",
   async ({ id, newData }) => {
     try {
       const response = await axios.put(`${url}/${id}`, newData);
@@ -39,7 +39,7 @@ export const updateTeacher = createAsyncThunk(
   }
 );
 
-export const deleteTeacher = createAsyncThunk("teacher/delete", async (id) => {
+export const deleteTeacher = createAsyncThunk("teachers/delete", async (id) => {
   try {
     const response = await axios.delete(`${url}/${id}`);
     return response.data;
@@ -58,60 +58,61 @@ export const teacherSlice = createSlice({
   name: "teachers",
   initialState,
   reducers: {},
-  extraReducers: {
-    [fetchTeachers.pending]: (state) => {
-      state.status = "loading";
-    },
-    [fetchTeachers.fulfilled]: (state, action) => {
-      state.status = "success";
-      state.teachers = action.payload;
-    },
-    [fetchTeachers.rejected]: (state, action) => {
-      state.status = "error";
-      state.error = action.error.message;
-    },
-    [addTeacher.pending]: (state) => {
-      state.status = "loading";
-    },
-    [addTeacher.fulfilled]: (state, action) => {
-      state.status = "success";
-      state.teachers.push(action.payload);
-    },
-    [addTeacher.rejected]: (state, action) => {
-      state.status = "error";
-      state.error = action.error.message;
-    },
-    [updateTeacher.pending]: (state) => {
-      state.status = "loading";
-    },
-    [updateTeacher.fulfilled]: (state, action) => {
-      state.status = "success";
-      const updatedTeacher = action.payload;
-      const index = state.teachers.findIndex(
-        (teacher) => teacher.id === updatedTeacher.id
-      );
-      if (index !== -1) {
-        state.teachers[index] = updatedTeacher;
-      }
-    },
-    [updateTeacher.rejected]: (state, action) => {
-      state.status = "error";
-      state.error = action.error.message;
-    },
-    [deleteTeacher.pending]: (state, action) => {
-      state.status = "loading";
-    },
-    [deleteTeacher.fulfilled]: (state, action) => {
-      state.status = "success";
-      const deletedTeacherId = action.payload.id;
-      state.teachers = state.teachers.filter(
-        (teacher) => teacher.id !== deletedTeacherId
-      );
-    },
-    [deleteTeacher.rejected]: (state, action) => {
-      state.status = "error";
-      state.error = action.error.message;
-    },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchTeachers.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchTeachers.fulfilled, (state, action) => {
+        state.status = "success";
+        state.teachers = action.payload;
+      })
+      .addCase(fetchTeachers.rejected, (state, action) => {
+        state.status = "error";
+        state.error = action.error.message;
+      })
+      .addCase(addTeacher.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(addTeacher.fulfilled, (state, action) => {
+        state.status = "success";
+        state.teachers.push(action.payload);
+      })
+      .addCase(addTeacher.rejected, (state, action) => {
+        state.status = "error";
+        state.error = action.error.message;
+      })
+      .addCase(updateTeacher.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(updateTeacher.fulfilled, (state, action) => {
+        state.status = "success";
+        const updatedTeacher = action.payload;
+        const index = state.teachers.findIndex(
+          (teacher) => teacher._id === updatedTeacher._id
+        );
+        if (index !== -1) {
+          state.teachers[index] = updatedTeacher;
+        }
+      })
+      .addCase(updateTeacher.rejected, (state, action) => {
+        state.status = "error";
+        state.error = action.error.message;
+      })
+      .addCase(deleteTeacher.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(deleteTeacher.fulfilled, (state, action) => {
+        state.status = "success";
+        const deletedTeacherId = action.payload._id;
+        state.teachers = state.teachers.filter(
+          (teacher) => teacher._id !== deletedTeacherId
+        );
+      })
+      .addCase(deleteTeacher.rejected, (state, action) => {
+        state.status = "error";
+        state.error = action.error.message;
+      });
   },
 });
 
